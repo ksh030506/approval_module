ae9dc48150e7ed3db15ba35fc72628c56d4a3619
ksh030506 <llmm030506@naver.com>
2022-02-19 19:11:13 +0900

    first commit

---
+++ src/main/java/co/kr/toss_approval/TossApprovalApplication.java
@@ -0,0 +1,13 @@
+package co.kr.toss_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossApprovalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/toss_approval/TossApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.toss_approval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossApprovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
