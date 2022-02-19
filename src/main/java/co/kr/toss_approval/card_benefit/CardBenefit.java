07d39723f34b4ca6550f3544f31ee17077bd22df
ksh030506 <llmm030506@naver.com>
2022-02-19 21:33:19 +0900

    Toss 카드 혜택 외부화

--- src/main/java/co/kr/toss_approval/card_benefit/CardBenefit.java
+++ src/main/java/co/kr/toss_approval/card_benefit/CardBenefit.java
@@ -6,11 +6,15 @@ import co.kr.toss_approval.domain.TossResult;
 import com.google.gson.Gson;
 import org.apache.hc.client5.http.fluent.Request;
 import org.apache.hc.client5.http.fluent.Response;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class CardBenefit implements Approval  {
+
+    @Value("toss.url.benefit-card")
+    private String BENEFIT_CARD_URL;
 
     @Override
     public TossResult<CardBenefitResponse> execute() throws IOException {
